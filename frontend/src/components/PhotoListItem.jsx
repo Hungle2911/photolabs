034b13034029972de8635bac6f8e7ea085bc9da1
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-05-26 14:53:41 -0400

    add class for styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,10 +6,14 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({props}) => {
   return (
     <div className="photo-list__item">
-    <img src={props.imageSource}  />
-    <img src={props.profile} />
-    <div>{props.username}</div>
-    <div>{props.location.city}, {props.location.country}</div>
+    <img src={props.imageSource} className="photo-list__image" />
+    <div className="photo-list__user-details">
+    <img src={props.profile} className="photo-list__user-profile"/>
+    <div className="photo-list__user-info">
+    <div >{props.username}</div>
+    <div className="photo-list__user-location">{props.location.city}, {props.location.country}</div>
+    </div>
+    </div>
     </div>
   )
 };
