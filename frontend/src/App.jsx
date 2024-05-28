75278879873d4281938af9d8d3cfb341018a124e
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-05-28 16:38:49 -0400

    feat: display a Notification for Favourited Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import topics from "mocks/topics";
-import photos from "mocks/photos";
+
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos}/>
+      <HomeRoute />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
-import photos from 'mocks/photos';
+import topics from "mocks/topics";
+import photos from "mocks/photos";
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
-const HomeRoute = ({topics, photos}) => {
+const HomeRoute = () => {
   const [like, setLike] = useState([]);
   const toggleLike = (id) => like.includes(id) ? setLike(like.filter(e => e !== id)) : setLike([...like, id]);
+  const isFavPhotoExist = like.length > 0
   return (
     <div className="home-route">
-      <TopNavigation topics={topics}/>
+      <TopNavigation topics={topics} isFavPhotoExist={isFavPhotoExist}/>
       <PhotoList like={like} toggleLike={toggleLike} photos={photos} />
     </div>
   );
